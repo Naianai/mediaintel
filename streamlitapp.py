bcd924b0b1b9d275c357cf287a6d748e0e6a2525
Naianai <qinaiardp@gmail.com>
2025-06-09 00:40:03 +0700

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -54,7 +54,6 @@
         .modal-content {
             background-color: #fefefe;
             margin: auto;
-            border-radius: 10px;
             box-shadow: 0 4px 8px rgba(0,0,0,0.2);
             width: 80%; /* Could be adjusted */
             max-width: 500px;
